2c0ad831f7623853834ab00b88d5ceee1d534317
Erkka Pynnönen <erkka@blacklabel.fi>
2017-04-16 22:35:55 +0300

    Added react-router and test route

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,21 @@
 import React, {Component} from 'react'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import Welcome from './Welcome'
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Welcome />
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li><Link to="/app">Welcome</Link></li>
+            </ul>
+          </nav>
+          <hr />
+          <Route path="/app" component={Welcome} />
+        </div>
+      </Router>
     )
   }
 }
