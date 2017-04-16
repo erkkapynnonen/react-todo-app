047630414036d7acef80823dfbd2cfc1c4c4e80d
Erkka Pynnönen <erkka@blacklabel.fi>
2017-04-16 22:52:08 +0300

    Fixed templates for bootstrap

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -10,11 +10,16 @@ export default class Welcome extends Component {
   }
   render() {
     return (
-      <div>
-        <h1>Welcome!</h1>
-        <p>This is a simple to-do app made with React for demo and learning purposes.</p>
-        <p>Check out the repo on <a target="_blank" href={this.state.appRepositoryUrl}>GitHub</a>.</p>
-        <p>You can use the demo account demo/demo for login as well as you Google/FB account via Oauth, provided by Auth0.</p>
+      <div className="row">
+        <div className="col-xs-12 col-md-6">
+          <h1>Welcome!</h1>
+          <p>This is a simple to-do app made with React for demo and learning purposes.</p>
+          <p>Check out the repo on <a target="_blank" href={this.state.appRepositoryUrl}>GitHub</a>.</p>
+          <p>You can use the demo account demo/demo for login as well as you Google/FB account via Oauth, provided by Auth0.</p>
+        </div>
+        <div className="col-xs-12 col-md-6">
+          <h3>Authenticate!</h3>
+        </div>
       </div>
     )
   }
--- src/components/app.js
+++ src/components/app.js
@@ -1,19 +1,13 @@
 import React, {Component} from 'react'
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Welcome from './Welcome'
 
 export default class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <nav>
-            <ul>
-              <li><Link to="/app">Welcome</Link></li>
-            </ul>
-          </nav>
-          <hr />
-          <Route path="/app" component={Welcome} />
+        <div className="container">
+          <Route path="/" component={Welcome} />
         </div>
       </Router>
     )
