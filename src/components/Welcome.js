9ac86e7d8048797696833b857f53b76267950a8c
Erkka Pynnönen <erkka@blacklabel.fi>
2017-04-16 20:58:27 +0300

    Added Welcome component

---
+++ src/components/Welcome.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+
+export default class Welcome extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      appRepositoryUrl: 'https://github.com/erkkapynnonen/react-todo-app'
+    };
+
+  }
+  render() {
+    return (
+      <div>
+        <h1>Welcome!</h1>
+        <p>This is a simple to-do app made with React for demo and learning purposes.</p>
+        <p>Check out the repo on <a target="_blank" href="{this.state.appRepositoryUrl}">GitHub</a>.</p>
+        <p>You can use the demo account demo/demo for login as well as you Google/FB account via Oauth, provided by Auth0.</p>
+      </div>
+    )
+  }
+}
--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react'
+import Welcome from './components/Welcome'
 
 export default class App extends Component {
   render() {
     return (
-      <div></div>
+      <div>
+        <Welcome />
+      </div>
     )
   }
 }
